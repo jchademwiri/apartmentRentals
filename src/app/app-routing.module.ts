56eb72853654dfbad1d1a7836a421bf1ebae7076
playhousegraphix <jchademwiri@gmail.com>
2020-05-26 15:03:14 +0200

    added responsive design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ApartmentsComponent } from './apartments/apartments.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: './', component: AppComponent },
+  { path: 'home', component: ApartmentsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [AppComponent, ApartmentsComponent, AboutComponent, ContactComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,27 +7,25 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ApartmentsComponent } from './apartments/apartments.component';
-import { AboutComponent } from './about/about.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { ContactComponent } from './contact/contact.component';
 
+// import { ApartmentsComponent } from './apartments/apartments.component';
+// import { AboutComponent } from './about/about.component';
+// import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ApartmentsComponent,
-    AboutComponent,
     NavbarComponent,
-    ContactComponent
+    routingComponents
   ],
   imports: [
     FormsModule,
